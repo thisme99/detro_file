c2c1a890861b2ead46eddf79bab527f470b8379b
thisme99 <31091609+thisme99@users.noreply.github.com>
2021-06-19 23:37:34 +0800

    Update filesafelink.js

--- filesafelink.js
+++ filesafelink.js
@@ -1,7 +1,7 @@
 var Base64 = {
-_keyStr: “ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=”,
+_keyStr: "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",
 encode: function (input) {
-var output = “”;
+var output = "";
 var chr1, chr2, chr3, enc1, enc2, enc3, enc4;
 var i = 0;
 input = Base64._utf8_encode(input);
@@ -23,11 +23,11 @@ output = output + this._keyStr.charAt(enc1) + this._keyStr.charAt(enc2) + this._
 return output;
 },
 decode: function (input) {
-var output = “”;
+var output = "";
 var chr1, chr2, chr3;
 var enc1, enc2, enc3, enc4;
 var i = 0;
-input = input.replace(/[^A-Za-z0-9\+\/\=]/g, “”);
+input = input.replace(/[^A-Za-z0-9\+\/\=]/g, "");
 while (i < input.length) {
 enc1 = this._keyStr.indexOf(input.charAt(i++));
 enc2 = this._keyStr.indexOf(input.charAt(i++));
@@ -48,8 +48,8 @@ output = Base64._utf8_decode(output);
 return output;
 },
 _utf8_encode: function (string) {
-string = string.replace(/\r\n/g, “\n”);
-var utftext = “”;
+string = string.replace(/\r\n/g, "\n");
+var utftext = "";
 for (var n = 0; n < string.length; n++) {
 var c = string.charCodeAt(n);
 if (c < 128) {
@@ -66,7 +66,7 @@ utftext += String.fromCharCode((c & 63) | 128);
 return utftext;
 },
 _utf8_decode: function (utftext) {
-var string = “”;
+var string = "";
 var i = 0;
 var c = c1 = c2 = 0;
 while (i < utftext.length) {
@@ -88,57 +88,55 @@ i += 3;
 return string;
 }
 }
-var encode = document.getElementById(‘encode’),
-decode = document.getElementById(‘decode’),
-output = document.getElementById(‘output’),
-input = document.getElementById(‘input’);
-var User_ID = “”;
-var protected_links = “”;
+var encode = document.getElementById('encode'),
+decode = document.getElementById('decode'),
+output = document.getElementById('output'),
+input = document.getElementById('input');
+var User_ID = "";
+var protected_links = "";
 var a_to_va = 0;
 var a_to_vb = 0;
-var a_to_vc = “”;
-
+var a_to_vc = "";
 function auto_safelink() {
 auto_safeconvert();
 }
-
 function auto_safeconvert() {
 var a_to_vd = window.location.hostname;
-if (protected_links != “” && !protected_links.match(a_to_vd)) {
-protected_links += “, ” + a_to_vd;
-} else if (protected_links == “”) {
+if (protected_links != "" && !protected_links.match(a_to_vd)) {
+protected_links += ", " + a_to_vd;
+} else if (protected_links == "") {
 protected_links = a_to_vd;
 }
-var a_to_ve = “”;
+var a_to_ve = "";
 var a_to_vf = new Array();
 var a_to_vg = 0;
-a_to_ve = document.getElementsByTagName(“a”);
+a_to_ve = document.getElementsByTagName("a");
 a_to_va = a_to_ve.length;
 a_to_vf = a_to_fa();
 a_to_vg = a_to_vf.length;
 var a_to_vh = false;
 var j = 0;
-var a_to_vi = “”;
+var a_to_vi = "";
 for (var i = 0; i < a_to_va; i++) {
 a_to_vh = false;
 j = 0;
 while (a_to_vh == false && j < a_to_vg) {
 a_to_vi = a_to_ve[i].href;
-if (a_to_vi.match(a_to_vf[j]) || !a_to_vi || !a_to_vi.match(“http”)) {
+if (a_to_vi.match(a_to_vf[j]) || !a_to_vi || !a_to_vi.match("http")) {
 a_to_vh = true;
 }
 j++;
 }
 if (a_to_vh == false) {
 var encryptedUrl = Base64.encode(a_to_vi);
-a_to_ve[i].href = “https://detronizator.org/safelink.html?url=” + encryptedUrl;
-a_to_ve[i].rel = “nofollow”;
+a_to_ve[i].href = "https://detronizator.org/safelink.html?url=" + encryptedUrl;
+a_to_ve[i].rel = "nofollow";
 a_to_vb++;
-a_to_vc += i + “:::” + a_to_ve[i].href + “\n”;
+a_to_vc += i + ":::" + a_to_ve[i].href + "\n";
 }
 }
-var a_to_vj = document.getElementById(“anonyminized”);
-var a_to_vk = document.getElementById(“found_links”);
+var a_to_vj = document.getElementById("anonyminized");
+var a_to_vk = document.getElementById("found_links");
 if (a_to_vj) {
 a_to_vj.innerHTML += a_to_vb;
 }
@@ -146,10 +144,9 @@ if (a_to_vk) {
 a_to_vk.innerHTML += a_to_va;
 }
 }
-
 function a_to_fa() {
 var a_to_vf = new Array();
-protected_links = protected_links.replace(” “, “”);
-a_to_vf = protected_links.split(“,”);
+protected_links = protected_links.replace(" ", "");
+a_to_vf = protected_links.split(",");
 return a_to_vf;
 }
